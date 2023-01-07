20741c632ed3fc6479fe28efc145f92736f11313
seungpyo <spkbk98@gmail.com>
2023-01-07 18:11:52 +0000

    Implemented simple train loop

--- cars196.py
+++ cars196.py
@@ -13,13 +13,7 @@ def get_dataset(root_dir: str) -> Dict[str, StanfordCars]:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="/data/stanford_cars")
+    parser.add_argument("--data_dir", type=str, default="/data/")
     args = parser.parse_args()
-    if os.path.exists(args.data_dir) and len(os.listdir(args.data_dir)) > 0:
-        opt = input(f"Data directory {args.data_dir} is not empty. Do you want to continue? [y/n]")
-        if opt.lower() != "y":
-            print("Exiting...")
-            exit(0)
     os.makedirs(args.data_dir, exist_ok=True)
-    train = StanfordCars(root=args.data_dir, split="train", download=True)
-    test = StanfordCars(root=args.data_dir, split="test", download=True)
+    get_dataset(args.data_dir)
--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 import pytorch_lightning as pl
-from .model import ModelType, EmbeddingModel
+from model import ModelType, EmbeddingModel
 import torch
 from torch.optim import AdamW
 from typing import *
 from dataclasses import dataclass
 from cars196 import get_dataset
-
+from PIL import Image
+import numpy as np
+import albumentations as A
+from torchvision.datasets import StanfordCars
 
 @dataclass
 class LiftedStructureEmbeddingConfig:
@@ -14,6 +17,7 @@ class LiftedStructureEmbeddingConfig:
     lr: float
     weight_decay: float
     max_epochs: int
+    batch_size: int
 
 class LiftedStructureEmbedding(pl.LightningModule):
     def __init__(self, config: LiftedStructureEmbeddingConfig) -> None:
@@ -47,6 +51,8 @@ class LiftedStructureEmbedding(pl.LightningModule):
             "scheduler": scheduler,
         }
         
+def albumentations_fn(transform: callable, img: Image.Image):
+    return transform(image=np.array(img))["image"]
 
 def main():
     '''
@@ -63,25 +69,39 @@ def main():
         lr=1e-3,
         weight_decay=1e-4,
         max_epochs=100,
+        batch_size=32,
     )
         
     module = LiftedStructureEmbedding(config=config)
+    tb_logger = pl.loggers.TensorBoardLogger("logs")
+    tb_logger.log_hyperparams(config.__dict__)
     
     trainer = pl.Trainer(
-        logger=pl.loggers.TensorBoardLogger("logs"),
+        logger=tb_logger,
         log_every_n_steps=1,
         max_epochs=config.max_epochs,
         accelerator="gpu",
     )
-    ds = get_dataset("/data/stanford_cars")
+    train_t = A.Compose([
+        A.Resize(256, 256),
+        A.RandomCrop(227, 227),
+        A.HorizontalFlip(p=0.5),
+    ])
+    test_t = A.Compose([
+        A.Resize(256, 256),
+        A.CenterCrop(227, 227),
+    ])
+    train_ds = StanfordCars(root="/data/", split="train", download=True, transform=lambda x: albumentations_fn(train_t, x))
+    test_ds = StanfordCars(root="/data/", split="test", download=True, transform=lambda x: albumentations_fn(test_t, x))
+    
     train_dl = torch.utils.data.DataLoader(
-        dataset=ds, 
-        batch_size=32, 
+        dataset=train_ds, 
+        batch_size=config.batch_size, 
         num_workers=4
     )
     test_dl = torch.utils.data.DataLoader(
-        dataset=ds,
-        batch_size=32,
+        dataset=test_ds,
+        batch_size=config.batch_size,
         num_workers=4
     )
     trainer.fit(module, train_dataloaders=train_dl, valid_dataloaders=test_dl)
